fbcc7f360f23b68cd8710639628c2db5cfaf2999
govindgcoder <govind.g.coder@gmail.com>
2026-06-01 18:17:42 +0000

    defined schemas for data

--- src/api.py
+++ src/api.py
@@ -1,3 +1,30 @@
 from engine import JSONDB
 
 DB = JSONDB("../users.json")
+
+from typing import Any, Dict, List
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class UserData(BaseModel):
+    id: int
+    name: str
+    age: int
+    department: str
+
+
+class Operator(BaseModel):
+    sign: str
+    value: Any
+
+
+class Filter(BaseModel):
+    field: str
+    condition: Operator
+
+
+class UpdateRequest(BaseModel):
+    new_vals: Dict[str, Any]
+    conditions: List[Filter]
